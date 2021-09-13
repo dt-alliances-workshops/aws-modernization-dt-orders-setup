b4dca93ab189e30ebc48937e906e54fc07d50379
robertjahn <rob.jahn@gmail.com>
2021-09-13 10:11:10 -0400

    comment out synthetic

--- workshop-config/cleanup-workshop-config.sh
+++ workshop-config/cleanup-workshop-config.sh
@@ -41,7 +41,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     run_monaco_delete workshop
     run_monaco_delete k8
     run_monaco_delete services-vm
-    run_monaco_delete synthetics
+    #run_monaco_delete synthetics  # if add this, need to adjust token permission instructions
 
     reset_custom_dynatrace_config
 
