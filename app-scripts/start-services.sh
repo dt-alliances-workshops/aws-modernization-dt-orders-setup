3f5475f7ddc7b41ff211bf0e37982f2752a23c7d
robertjahn <rob.jahn@gmail.com>
2021-10-12 09:39:51 -0700

    add docker ps for more logging

--- app-scripts/start-services.sh
+++ app-scripts/start-services.sh
@@ -13,6 +13,12 @@ sudo $APP_SCRIPTS_PATH/stop-services.sh >> $LOGFILE 2>&1
 printf "\n\n***** Starting Services ***\n" >> $LOGFILE 2>&1
 sudo docker-compose -f "$APP_SCRIPTS_PATH/docker-compose-services.yaml" up -d >> $LOGFILE 2>&1
 
+printf "\n\n***** Sleeping 20 seconds ***\n" >> $LOGFILE 2>&1
+sleep 20
+
+printf "\n\n***** Getting docker processes ***\n" >> $LOGFILE 2>&1
+sudo docker ps >> $LOGFILE 2>&1
+
 END_TIME="$(date)"
 printf "\n\n" >> $LOGFILE 2>&1
 printf "\n\nSTART_TIME: $START_TIME     END_TIME: $END_TIME" >> $LOGFILE 2>&1
